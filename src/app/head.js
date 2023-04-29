c81d8cf01c80cf1f456f18c0cbb94e09e8aaf0b8
polooner <114031148+polooner@users.noreply.github.com>
2023-04-28 22:54:21 -0400

    Delete src directory

--- src/app/head.js
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>Next.js with Supabase Auth</title>
-      <link rel="icon" href="/favicon.ico" />
-      <meta name="viewport" content="width=device-width" />
-    </>
-  );
-}
