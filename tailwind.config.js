c3d85c10f97384435d741635db4c1b638ebbce16
polooner <114031148+polooner@users.noreply.github.com>
2023-04-28 22:55:30 -0400

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-const forms = require('@tailwindcss/forms');
-
-module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'media',
-  plugins: [forms],
-};
